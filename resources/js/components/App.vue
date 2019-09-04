c92773dfd4b837f37636a1394860c9b111b2925c
Daniel Sias <daniel.sias@gmail.com>
2019-09-04 16:29:25 -0400

    Base app entry point

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Hello from App</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'nexus',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
