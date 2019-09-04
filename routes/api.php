65f13e0bb88d54d5f404422d0eebf864a712283d
Daniel Sias <daniel.sias@gmail.com>
2019-09-03 23:42:05 -0400

    Program routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Programs and Code Mapping
+Route::get('/programs', 'ProgramController@index');
+Route::post('/programs', 'ProgramController@store');
+Route::patch('/programs/{program}', 'ProgramController@update');
